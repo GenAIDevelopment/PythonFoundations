94c9ee994c704e5d7790d8b69d2eb0457f4ce1f4
shaikkhajaibrahim <shaikkhajaibrahim@live.in>
2025-03-05 10:12:15 +0530

    Added from

--- basics/feb25/modules/sample1/main.py
+++ basics/feb25/modules/sample1/main.py
@@ -1,9 +1,9 @@
 """This module explains import of functions from other modules
 """
-import utils
+from utils import is_prime
 
 number = int(input("Enter the number of your choice "))
-if utils.is_prime(number):
+if is_prime(number):
     print(f"{number} is prime")
 else:
     print(f"{number} is not prime")
