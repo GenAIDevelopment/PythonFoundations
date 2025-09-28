d08549d3aedad63ba48bcec5928f513be26bd460
shaikkhajaibrahim <shaikkhajaibrahim@gmail.com>
2025-09-28 08:07:56 +0530

    Added cli v2

--- basics/sep25/modules/cli_calculator/calcv2.py
+++ basics/sep25/modules/cli_calculator/calcv2.py
@@ -14,6 +14,23 @@ calc.py mod 5 4
 import argparse
 supported_ops = ["add", "sub", "mul", "div", "mod"]
 
+def operation(args: argparse.Namespace):
+    """This method performs the operation  and prints the result
+
+    Args:
+        args (argparse.Namespace): _description_
+    """
+    if args.operation == "add":
+        print(args.value1 + args.value2)
+    elif args.operation == "sub":
+        print(args.value1 - args.value2)
+    elif args.operation == "mul":
+        print(args.value1 * args.value2)
+    elif args.operation == "div":
+        print(args.value1 / args.value2)
+    elif args.operation == "mod":
+        print(args.value1 % args.value2)
+
 
 def main():
     """Entry point
@@ -23,7 +40,8 @@ def main():
     parser.add_argument("value1", type=int)
     parser.add_argument("value2", type=int)
     args = parser.parse_args()
-    print(args)
+    operation(args)
+    
 
 if __name__ == '__main__':
     main()
